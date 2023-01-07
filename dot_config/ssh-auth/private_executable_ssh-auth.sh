b99465e0a280619d80a739e6df49e8d449f5590b
Hendrik <john.dalmas999@gmail.com>
2023-01-07 10:52:35 +0100

    fixed ssh agent sock check

--- dot_config/ssh-auth/private_executable_ssh-auth.sh
+++ dot_config/ssh-auth/private_executable_ssh-auth.sh
@@ -5,11 +5,13 @@ export SSH_AUTH_SOCK=$HOME/bin/agent.sock
 
 ALREADY_RUNNING=$(ps -auxww | grep -q "[n]piperelay.exe -ei -s //./pipe/openssh-ssh-agent"; echo $?)
 if [ $ALREADY_RUNNING -ne 0 ]; then
+    echo "no npiperelay process"
     rm -f $SSH_AUTH_SOCK
     echo "Create SSH agent pipe"
     (setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"$RELAY -ei -s //./pipe/openssh-ssh-agent",nofork &) >/dev/null 2>&1
 else 
-    if [ ! -f SSH_AUTH_SOCK ]; then
+    if [ ! -S $SSH_AUTH_SOCK ]; then
+        echo "no $SSH_AUTH_SOCK"
         echo "Create SSH agent pipe"
         (setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"$RELAY -ei -s //./pipe/openssh-ssh-agent",nofork &) >/dev/null 2>&1
     fi
