5250e1215b74f22f0a47a938e1002bdcfc742a7e
Hendrik <john.dalmas999@gmail.com>
2023-01-07 11:47:15 +0100

    improved ssh agent sock check

--- dot_config/ssh-auth/private_executable_ssh-auth.sh
+++ dot_config/ssh-auth/private_executable_ssh-auth.sh
@@ -1,18 +1,18 @@
 # Configure ssh forwarding
 
-RELAY=~/bin/npiperelay.exe
+# RELAY=~/bin/npiperelay.exe
+RELAY=npiperelay.exe
 export SSH_AUTH_SOCK=$HOME/bin/agent.sock
 
 ALREADY_RUNNING=$(ps -auxww | grep -q "[n]piperelay.exe -ei -s //./pipe/openssh-ssh-agent"; echo $?)
 if [ $ALREADY_RUNNING -ne 0 ]; then
-    echo "no npiperelay process"
+    # echo "no npiperelay process"
     rm -f $SSH_AUTH_SOCK
-    echo "Create SSH agent pipe"
+    echo "Create SSH agent pipe [no process]"
     (setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"$RELAY -ei -s //./pipe/openssh-ssh-agent",nofork &) >/dev/null 2>&1
-else 
-    if [ ! -S $SSH_AUTH_SOCK ]; then
-        echo "no $SSH_AUTH_SOCK"
-        echo "Create SSH agent pipe"
-        (setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"$RELAY -ei -s //./pipe/openssh-ssh-agent",nofork &) >/dev/null 2>&1
-    fi
+fi 
+if [ ! -S $SSH_AUTH_SOCK ]; then
+    # echo "no $SSH_AUTH_SOCK"
+    echo "Create SSH agent pipe [missing socket]"
+    (setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"$RELAY -ei -s //./pipe/openssh-ssh-agent",nofork &) >/dev/null 2>&1
 fi
