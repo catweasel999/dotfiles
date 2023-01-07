c8c2562fa1ae4e3dc5bbdea0ecf058871a8b881d
Hendrik <john.dalmas999@gmail.com>
2023-01-07 10:24:29 +0100

    better ssh agent piperelay

--- dot_config/ssh-auth/private_executable_ssh-auth.sh
+++ dot_config/ssh-auth/private_executable_ssh-auth.sh
@@ -8,4 +8,9 @@ if [ $ALREADY_RUNNING -ne 0 ]; then
     rm -f $SSH_AUTH_SOCK
     echo "Create SSH agent pipe"
     (setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"$RELAY -ei -s //./pipe/openssh-ssh-agent",nofork &) >/dev/null 2>&1
+else 
+    if [ ! -f SSH_AUTH_SOCK ]; then
+        echo "Create SSH agent pipe"
+        (setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"$RELAY -ei -s //./pipe/openssh-ssh-agent",nofork &) >/dev/null 2>&1
+    fi
 fi
