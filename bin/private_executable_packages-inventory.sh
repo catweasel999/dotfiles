d11aa87ae5fbe89abaf1a4d0c3ba118a5182ba51
Hendrik <john.dalmas999@gmail.com>
2023-01-12 09:00:03 +0100

    ubuntu package list

---
+++ bin/private_executable_packages-inventory.sh
@@ -0,0 +1,19 @@
+
+#!/bin/bash
+
+TARGET_DIR=~/downloads
+mkdir -p $TARGET_DIR
+
+if ! command -v dpkg-query &> /dev/null
+then
+    PACKAGE_FILE=dpkg_packages.txt
+    TARGET_FILE=$TARGET_DIR/$PACKAGE_FILE
+    echo "dpkg-query found - creating package list."
+    sudo dpkg-query -f '${binary:Package}\n' -W > $TARGET_FILE
+    echo "Package list created: $TARGET_FILE"
+    exit 0
+else
+    echo "dpkg-query not found!"
+    exit 1
+fi
+
