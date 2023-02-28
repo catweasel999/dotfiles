cce59ebe332cd36db1e69fc9864b86026a6ae8a7
Hendrik <john.dalmas999@gmail.com>
2023-02-28 10:29:44 +0100

    ~/bin scripts

--- bin/private_executable_packages-inventory.sh
+++ bin/private_executable_packages-inventory.sh
@@ -1,19 +1,16 @@
-
 #!/bin/bash
 
 TARGET_DIR=~/downloads
 mkdir -p $TARGET_DIR
 
-if ! command -v dpkg-query &> /dev/null
-then
+if command -v dpkg-query &>/dev/null; then
     PACKAGE_FILE=dpkg_packages.txt
     TARGET_FILE=$TARGET_DIR/$PACKAGE_FILE
     echo "dpkg-query found - creating package list."
-    sudo dpkg-query -f '${binary:Package}\n' -W > $TARGET_FILE
+    sudo dpkg-query -f '${binary:Package}\n' -W >$TARGET_FILE
     echo "Package list created: $TARGET_FILE"
     exit 0
 else
     echo "dpkg-query not found!"
     exit 1
 fi
-
