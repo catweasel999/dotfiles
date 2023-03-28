7a10df1b5f9419b23af63f50e993271184391894
Hendrik <john.dalmas999@gmail.com>
2023-03-28 11:08:00 +0200

    ignition models only if ign installed

--- dot_config/private_ros.org/private_executable_ros_env.sh
+++ dot_config/private_ros.org/private_executable_ros_env.sh
@@ -1,7 +1,10 @@
 PROJECT=udemy_ros2_ws/src/udemy_ros2_pkg
 
-workspace_candidates=(/workspace ~/source)
-for i in "${workspace_candidates[@]}"; do
-    MODEL_FOLDER=$i/$PROJECT/models
-    [ -d $MODEL_FOLDER ] && export IGN_GAZEBO_RESOURCE_PATH=$MODEL_FOLDER
-done
+# if ignition is installed, look for a local model folder
+if command -v ign &>/dev/null; then
+    workspace_candidates=(/workspace ~/source)
+    for i in "${workspace_candidates[@]}"; do
+        MODEL_FOLDER=$i/$PROJECT/models
+        [ -d $MODEL_FOLDER ] && export IGN_GAZEBO_RESOURCE_PATH=$MODEL_FOLDER
+    done
+fi
