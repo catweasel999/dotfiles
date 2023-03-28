1bc5eee10afb3e2010035978700f718b4793c9d3
Hendrik <john.dalmas999@gmail.com>
2023-03-28 11:41:01 +0200

    ign check fixed ws folder

--- dot_config/private_ros.org/private_executable_ros_env.sh
+++ dot_config/private_ros.org/private_executable_ros_env.sh
@@ -2,7 +2,7 @@ PROJECT=udemy_ros2_ws/src/udemy_ros2_pkg
 
 # if ignition is installed, look for a local model folder
 if command -v ign 2>&1 >/dev/null; then
-    workspace_candidates=(/workspace ~/source)
+    workspace_candidates=(/workspaces ~/source)
     for i in "${workspace_candidates[@]}"; do
         MODEL_FOLDER=$i/$PROJECT/models
         [ -d $MODEL_FOLDER ] && export IGN_GAZEBO_RESOURCE_PATH=$MODEL_FOLDER
