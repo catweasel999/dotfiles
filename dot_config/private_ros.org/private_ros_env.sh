d009268b8e38b10791cbf5d2722e767b0b656e52
Hendrik <john.dalmas999@gmail.com>
2024-06-22 10:47:33 +0200

    fix wrong substitution

--- dot_config/private_ros.org/private_ros_env.sh
+++ dot_config/private_ros.org/private_ros_env.sh
@@ -1,5 +1,8 @@
-[ - f ros-local.sh ] && . ros-local.sh
+CWD=$(dirname "$0")
+LOCAL_CONFIG=$CWD/ros-local.sh
 
-export ROS_DISTRO=S{ROS_DISTRO:-"iron"}
+[[ -f $LOCAL_CONFIG ]] && source $LOCAL_CONFIG
+
+export ROS_DISTRO=${ROS_DISTRO:-"iron"}
 export ROS_DOMAIN_ID=1
 export ROS_AUTOMATIC_DISCOVERY_RANGE=SUBNET
