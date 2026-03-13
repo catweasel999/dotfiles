5d4ca5c1b14bcd136664b7764864eada140fabfb
Hendrik <john.dalmas999@gmail.com>
2026-03-13 16:30:46 +0100

    ros2 jazzy

--- dot_config/private_ros.org/private_ros_env.sh
+++ dot_config/private_ros.org/private_ros_env.sh
@@ -3,6 +3,6 @@ LOCAL_CONFIG=$CWD/ros-local.sh
 
 [[ -f $LOCAL_CONFIG ]] && source $LOCAL_CONFIG
 
-export ROS_DISTRO=${ROS_DISTRO:-"iron"}
+export ROS_DISTRO=${ROS_DISTRO:-"jazzy"}
 export ROS_DOMAIN_ID=1
 export ROS_AUTOMATIC_DISCOVERY_RANGE=SUBNET
