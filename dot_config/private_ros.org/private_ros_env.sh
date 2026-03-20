65fbd7582ebb5551eb373dc87217709653e22899
Hendrik <john.dalmas999@gmail.com>
2026-03-20 08:14:45 +0100

    remove ros environment vars

--- dot_config/private_ros.org/private_ros_env.sh
+++
@@ -1,8 +0,0 @@
-CWD=$(dirname "$0")
-LOCAL_CONFIG=$CWD/ros-local.sh
-
-[[ -f $LOCAL_CONFIG ]] && source $LOCAL_CONFIG
-
-export ROS_DISTRO=${ROS_DISTRO:-"jazzy"}
-export ROS_DOMAIN_ID=1
-export ROS_AUTOMATIC_DISCOVERY_RANGE=SUBNET
