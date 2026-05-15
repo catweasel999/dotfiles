d391b74280fd3a9e7241482d6af98439731d42d1
Hendrik <john.dalmas999@gmail.com>
2026-05-15 10:36:35 +0200

    add sudo esc/esc

---
+++ bin/private_executable_stop-wd-automounts.sh
@@ -0,0 +1,5 @@
+sudo systemctl stop home-catweasel-Cloud-wdmycloudmirror-sonja.automount
+sudo systemctl stop home-catweasel-Cloud-wdmycloudmh-catweasel.automount
+sudo systemctl stop home-catweasel-Cloud-wdmycloudmirror-catweasel.automount
+
+systemctl list-automounts --all
